6aabc32309b77991a619dece47eceea8f41cc93d
nexlice <auspiciouswho47@gmail.com>
2022-11-16 00:14:40 +0900

    feat: TickState interface

--- HolubLife/src/com/holub/life/Go.java
+++ HolubLife/src/com/holub/life/Go.java
@@ -2,10 +2,6 @@ package com.holub.life;
 
 public enum Go {
 	
-	//여기여기여기
-	// (TODO)
-	// enum 을 클래스로 만들어서 state pattern을 적용할 수 있지않을까?
-	
 	Halt		(0, "Halt"), 
 	Tick 		(0, "Tick (Single Step)"),
 	Agonizing	(500, "Agonizing"),	
@@ -13,8 +9,12 @@ public enum Go {
 	Medium		(70, "Medium"),
 	Fast		(50, "Fast");
 	
+	
+	
 	private final int tick;
 	private final String name;
+	
+	
 	
 	Go(int tick, String name) {
 		// TODO Auto-generated constructor stub
