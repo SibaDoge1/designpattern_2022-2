99cb262f0ea3a0209eb5aa361b11e47577b465f6
nexlice <auspiciouswho47@gmail.com>
2022-11-27 16:33:13 +0900

    fix: Access modifier

--- HolubLife/src/com/holub/life/Go.java
+++ HolubLife/src/com/holub/life/Go.java
@@ -2,7 +2,6 @@ package com.holub.life;
 
 import java.awt.event.ActionEvent;
 
-import javax.swing.JMenuItem;
 import javax.swing.JTextField;
 
 public class Go {
@@ -17,18 +16,18 @@ public class Go {
 		this.TD = TD;
 	}
 	
-	public void setState(TickState state) {
+	private void setState(TickState state) {
 		
 		this.CurrentState = state;
 
 	}
 	
-	public void setE(ActionEvent e) {
+	private void setE(ActionEvent e) {
 		
 		this.e = e;
 	}
 
-	public void doAction() {
+	private void doAction() {
 		
 		if (CurrentState instanceof CustomState) {
 			JTextField tmp = (JTextField)e.getSource();
